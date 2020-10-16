76b6f2ae98016c1b112b12cebf3342be0a2c97fc
Gabriel South <gabriel.inchoste@southsystem.com.br>
2020-10-16 14:49:12 -0300

    master

---
+++ src/main/java/br/com/gabrieldias/agendaTCC/AgendaTccApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrieldias.agendaTCC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaTccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaTccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabrieldias/agendaTCC/AgendaTccApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrieldias.agendaTCC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaTccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
